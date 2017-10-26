df7d0b87779ac4b030ae8836d7ef82013da03bcc
Joanne Cheng <joanne@keen.io>
2017-10-26 13:07:39 -0600

    import data from another file

--- src/cljs/color-analysis/core.cljs
+++ src/cljs/color-analysis/core.cljs
@@ -1,28 +1,14 @@
 (ns color-analysis.core
-  (:require [cljsjs.d3]))
+  (:require [cljsjs.d3]
+            [color-analysis.data]))
+
 
 (enable-console-print!)
+(declare simulation)
 
 ;(set! (.-innerHTML (.getElementById js/document "app")) "<h1>Circles?</h1>")
 
-(def values [
-             { :color "#1e0821" :value 685 }
-             { :color "#100211" :value 174 }
-             { :color "#545876" :value 392 }
-             { :color "#4f1141" :value 497 }
-             { :color "#0a010d" :value 97  }
-             { :color "#9cadb4" :value 278 }
-             { :color "#190518" :value 333 }
-             { :color "#100316" :value 232 }
-             { :color "#a95a88" :value 386 }
-             { :color "#241227" :value 517 }
-             { :color "#331836" :value 585 }
-             { :color "#a91255" :value 351 }
-             { :color "#15051d" :value 218 }
-             { :color "#781350" :value 489 }
-             { :color "#2c082d" :value 574 }
-             { :color "#090313" :value 132 }
-          ])
+(def values (get (nth (color-analysis.data/values) 1) :values))
 
 (def width 960)
 (def height 500)
@@ -40,6 +26,7 @@
 
 
 (defn ticked []
+  ;change to let?
   (def circles (.. svg
              (selectAll "circle")
              (data (clj->js (.nodes simulation)))))
---
+++ src/cljs/color-analysis/data.cljs
@@ -0,0 +1,43 @@
+(ns color-analysis.data)
+
+(defn values [] [
+  {
+   :name 203
+   :values [{ :color "#1e0821" :value 685 }
+            { :color "#100211" :value 174 }
+            { :color "#545876" :value 392 }
+            { :color "#4f1141" :value 497 }
+            { :color "#0a010d" :value 97  }
+            { :color "#9cadb4" :value 278 }
+            { :color "#190518" :value 333 }
+            { :color "#100316" :value 232 }
+            { :color "#a95a88" :value 386 }
+            { :color "#241227" :value 517 }
+            { :color "#331836" :value 585 }
+            { :color "#a91255" :value 351 }
+            { :color "#15051d" :value 218 }
+            { :color "#781350" :value 489 }
+            { :color "#2c082d" :value 574 }
+            { :color "#090313" :value 132 }
+            ]
+   }
+  {
+   :name 341
+   :values [{ :color "#08279a" :value 724 }
+            { :color "#061d90" :value 767 }
+            { :color "#070950" :value 345 }
+            { :color "#185ad5" :value 404 }
+            { :color "#1503a3" :value 77  }
+            { :color "#0c35ad" :value 664 }
+            { :color "#7caef7" :value 53 }
+            { :color "#2c1c26" :value 42 }
+            { :color "#2475e9" :value 257 }
+            { :color "#090a62" :value 402 }
+            { :color "#05137b" :value 698 }
+            { :color "#04116c" :value 530 }
+            { :color "#100a13" :value 162 }
+            { :color "#1146c1" :value 515 }
+            { :color "#3854b7" :value 66 }
+            { :color "#090837" :value 234 }
+            ]
+   } ])
